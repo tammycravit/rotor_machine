f950944d3d9c081bb72d18a818fe6841bd6dfded
Tammy Cravit <tammycravit@me.com>
2018-02-08 10:55:37 -0500

    Added link to Kevin Sylvestre blog post to documentation

--- lib/rotor_machine.rb
+++ lib/rotor_machine.rb
@@ -17,6 +17,10 @@ end
 #
 # The documentation for {RotorMachine::Machine} shows an example of how to
 # use the module.
+#
+# Many thanks to Kevin Sylvestre, whose {https://ksylvest.com/posts/2015-01-03/the-enigma-machine-using-ruby blog post}
+# helped me understand some aspects of the internal workings of the Enigma
+# and how the signals flowed through the pieces of the machine.
 # 
 #@author Tammy Cravit <tammycravit@me.com>
 module RotorMachine
