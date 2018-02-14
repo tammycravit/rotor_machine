420822ee34051842bf74dd2d5dc66d22c72e4703
Tammy Cravit <tammycravit@me.com>
2018-02-14 01:29:59 -0500

    Minor fixes to rotor_state_matcher

--- spec/support/matchers/rotor_state_matcher.rb
+++ spec/support/matchers/rotor_state_matcher.rb
@@ -1,3 +1,24 @@
+############################################################################
+# rotor_machine: Simple ruby implemenation of an Enigma rotor @machine.
+#
+# File        : rotor_state_matcher.rb
+# Description : RSpec custom matcher to make validating Rotor objects easier.
+############################################################################
+#  Copyright 2018, Tammy Cravit.
+# 
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+############################################################################
+
 require 'rspec/expectations'
 
 RSpec::Matchers.define :have_rotor_state do |options|
